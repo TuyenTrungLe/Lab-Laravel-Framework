bd2bf3340ccfbd9a31376938e205b4c185ee2e79
Tuyen Trung Le <letrungtuyen2002@gmail.com>
2024-12-20 11:44:41 +0700

    Completed Section 1 - Working with Database

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    public const DEFAULT_CURRENCY = 'VNĐ';
+
+    public $products = [
+        [
+            'id' => 1,
+            'category_id' => 1,
+            'name' => 'Cappuchino',
+            'description' => 'Cappuccino is a coffee drink...',
+            'price' => 35000,
+            'display_image_url' => Product::DEFAULT_IMAGE,
+        ],
+        [
+            'id' => 2,
+            'category_id' => 2,
+            'name' => 'Chicken satay salad',
+            'description' => 'Marinate chicken breasts...',
+            'price' => 50000,
+            'display_image_url' => Product::DEFAULT_IMAGE,
+        ],
+    ];
+
+    private function getProducts()
+    {
+        return collect($this->products);
+    }
+
+    public function run(): void
+    {
+        foreach ($this->getProducts() as $product) {
+            $product['currency'] = self::DEFAULT_CURRENCY;
+            Product::create($product);
+        }
+    }
+}
